d1db19b1d27f39e6fa96403258fd0eca4563a4d8
Stian Andre Olsen <stian.privat@gmail.com>
2024-12-08 21:53:05 +0100

    Moved to Visual Studio to be able to debug. Still problems with part two of Day 6. Can't understand why.
    Signed-off-by: Stian Andre Olsen <stian.privat@gmail.com>

--- 2024/src/Applications/Day6/Day6.cpp
+++ 2024/src/Applications/Day6/Day6.cpp
@@ -18,6 +18,9 @@ struct Direction
 	int di = 0;
 	int dj = 0;
 	Direction(int dx, int dy) : di(dx), dj(dy) {}
+	bool operator==(const Direction& rhs) const { return di == rhs.di && dj == rhs.dj; }
+	bool operator!=(const Direction& rhs) const { return !(*this == rhs); }
+	bool operator<(const Direction& rhs) const { return !(*this == rhs); }
 };
 
 Direction turnRight(Direction dir)
@@ -30,6 +33,16 @@ Position operator+(Position p, Direction d)
 	return Position(p.i + d.di, p.j + d.dj);
 }
 
+struct Pose
+{
+	Position pos;
+	Direction dir;
+	Pose(Position p, Direction d) : pos(p), dir(d) {}
+	bool operator==(const Pose& rhs) const { return pos == rhs.pos && dir == rhs.dir; }
+	bool operator!=(const Pose& rhs) const { return !(*this == rhs); }
+	bool operator<(const Pose& rhs) const { return pos < rhs.pos || dir < rhs.dir; }
+};
+
 class Map
 {
 	size_t ncols = 0;
@@ -38,12 +51,13 @@ public:
 	Map(const std::vector<std::string>& lines);
 	size_t ni() const { return ncols; }
 	size_t nj() const { return rows.size(); }
+	bool isValid(size_t i, size_t j) const { return i < ni() && j < nj(); }
 	char val(size_t i, size_t j) const { return rows[j][i]; }
 	void setVal(size_t i, size_t j, char val) { rows[j][i] = val; }
+	bool isValid(Position p) const { return isValid(p.i, p.j); }
 	char val(Position p) const { return val(p.i, p.j); }
 	void setVal(Position p, char val) { setVal(p.i, p.j, val); }
 	Position find(char val) const;
-	bool isValid(Position p) const { return p.i < ni() && p.j < nj(); }
 };
 
 Map::Map(const std::vector<std::string>& lines)
@@ -66,14 +80,18 @@ Position Map::find(char val) const
 		if (i != std::string::npos)
 			return Position(i,j);
 	}
-	return Position(-1,-1);
+	return Position(std::string::npos, std::string::npos);
 }
 
 Position next(const Map& map, Position pos, Direction& dir)
 {
-	if (map.val(pos + dir) == '#')
+	auto next = pos + dir;
+	if (map.isValid(next) && map.val(next) == '#')
+	{
 		dir = turnRight(dir);
-	return pos + dir;
+		next = pos + dir;
+	}
+	return next;
 }
 
 int main(int /*argc*/, char* /*argv*/[])
@@ -85,8 +103,11 @@ int main(int /*argc*/, char* /*argv*/[])
 		lines.push_back(line);
 	Map map(lines);
 
-	// part 1
+	// find start position and flag as a position without any obstructions
 	Position p0 = map.find('^');
+	map.setVal(p0, '.');
+
+	// part 1
 	Direction dir(0, -1); // start towards north
 	std::set<Position> positions;
 	for (Position p = p0; map.isValid(p); p = next(map, p, dir))
@@ -94,23 +115,26 @@ int main(int /*argc*/, char* /*argv*/[])
 	std::cout << "The guard will visit " << positions.size() << " distinct positions" << std::endl;
 
 	// part 2
-	size_t mapPositions = map.ni() * map.nj();
 	size_t obstructions = 0;
 	for (auto pos : positions)
+//	for (size_t j = 0; j < map.nj(); ++j)
+//	for (size_t i = 0; i < map.ni(); ++i)
 	{
-		if (pos != p0)
+//		Position pos(i, j);
+		if (pos != p0 && map.val(pos) == '.')
 		{
-			std::cout << pos.i << '\t' << pos.j << std::endl;
 			map.setVal(pos, '#');
-			Direction dir(0, -1); // start towards north
-			size_t steps = 0;
+			dir = Direction(0, -1); // start towards north
+			std::set<Pose> poses;
 			for (Position p = p0; map.isValid(p); p = next(map, p, dir))
 			{
-				if (++steps > mapPositions)
+				Pose pose(p, dir);
+				if (poses.count(pose) > 0)
 				{
 					++obstructions;
 					break;
 				}
+				poses.insert(pose);
 			}
 			map.setVal(pos, '.');
 		}
